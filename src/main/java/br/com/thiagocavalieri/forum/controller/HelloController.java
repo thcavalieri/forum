143b17ff22fb2a2f62f8576915e069a5a34f6aec
Thiago Cavalieri da Silveira <thcavalieri@gmail.com>
2021-04-29 16:45:02 -0300

    Starting project

---
+++ src/main/java/br/com/thiagocavalieri/forum/controller/HelloController.java
@@ -0,0 +1,15 @@
+package br.com.thiagocavalieri.forum.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "HelloController";
+    }
+}
