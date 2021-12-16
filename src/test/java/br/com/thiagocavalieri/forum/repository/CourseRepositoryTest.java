a75c16126abb67a4761460bb55dcb0f1e5e53a4f
Thiago Cavalieri da Silveira <thcavalieri@gmail.com>
2021-12-16 19:50:14 -0300

    Adding a new yaml for test profile.

--- src/test/java/br/com/thiagocavalieri/forum/repository/CourseRepositoryTest.java
+++ src/test/java/br/com/thiagocavalieri/forum/repository/CourseRepositoryTest.java
@@ -1,10 +1,13 @@
 package br.com.thiagocavalieri.forum.repository;
 
+import br.com.thiagocavalieri.forum.model.Course;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @DataJpaTest
 public class CourseRepositoryTest {
 
@@ -14,7 +17,14 @@ public class CourseRepositoryTest {
     @Test
     public void findCourseByNameTest() {
         var courseName = "HTML 5";
-        var course = courseRepository.findByName(courseName);
+
+        var course = new Course();
+        course.setName(courseName);
+        course.setCategory("Front-end");
+
+        courseRepository.save(course);
+
+        course = courseRepository.findByName(courseName);
 
         Assertions.assertNotNull(course);
         Assertions.assertEquals(courseName, course.getName());
